5ed9106b813003c885332f795baa56a74c70b134
Madhu Ranjan <96232326+Madhuranjansharma48@users.noreply.github.com>
2024-01-29 08:13:34 +0530

    client updated

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -2,6 +2,8 @@ import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import { Helmet } from "react-helmet";
+import { Toaster } from "react-hot-toast";
+
 const Layout = ({ children, title, description, keywords, author }) => {
   return (
     <div>
@@ -14,6 +16,9 @@ const Layout = ({ children, title, description, keywords, author }) => {
       </Helmet>
       <Header />
       <main style={{ minHeight: "70vh" }}>{children}</main>
+      <Toaster />
+      
+
       <Footer />
     </div>
   );
